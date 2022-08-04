b633585793a97d0f6d0998019db48252c654af0b
Betini Akarandut <betiniakarandut@gmail.com>
2022-08-04 06:46:13 +0100

    my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='binormal_distributions',
+      version='1.1',
+      description='Binomial and Gaussian distributions',
+      packages=['binormal_distributions'],
+      author='Betini Akarandut',
+      author_email='betiniakarandut@gmail.com',
+      zip_safe=False)
+
